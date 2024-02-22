f8d353d715054bf1dd20225a8fa11af38ae7c262
lkbrian <lkbrian742@gmail.com>
2024-02-22 07:24:20 +0300

    {Feature{: implimenting the gallery

---
+++ src/assets/galleryImages/images.js
@@ -0,0 +1,28 @@
+import videoFile from './0av.mp4';
+import od from './0d.jpg'
+import oda from './0e.jpg'
+import odb from './0f2.jpg'
+import odv from './0dv.mp4'
+import odc from './0f3.jpg'
+import odd from './0f4.jpg'
+import ode from './0f5.jpg'
+import odf from './0f6.jpg'
+import odg from './0f7.jpg'
+
+
+export const images = [
+    { file: videoFile, type: 'video' },
+    { file: od, type: 'image' },
+    { file: oda, type: 'image' },
+    { file: odb, type: 'image' },
+    { file: odv, type: 'video' },
+    { file: odc, type: 'image' },
+    { file: odd, type: 'image' },
+    { file: ode, type: 'image' },
+    { file: odf, type: 'image' },
+    { file: odg, type: 'image' },    
+    { file: videoFile, type: 'video' },
+    
+];
+
+
